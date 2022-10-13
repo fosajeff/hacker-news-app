baba706b2e565193b0fa6faa2e515ab4a8933258
Efosa <okoobohefosa@gmail.com>
2022-10-13 16:24:41 +0100

    remove auto seed from app

--- api/apps.py
+++ api/apps.py
@@ -4,10 +4,3 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-
-    def ready(self):
-        from .helpers import run_db_seeder
-        from .models import Item
-        qs = Item.objects.all()
-        if not qs.exists():
-            run_db_seeder()
